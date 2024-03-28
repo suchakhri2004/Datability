f175d0e99c04f8f791f61010696d3c2ed8d38640
suchakhri <121075817+suchakhri2004@users.noreply.github.com>
2024-03-28 18:08:17 +0700

    Update Task15.ts

--- src/Task15.ts
+++ src/Task15.ts
@@ -1,65 +1,29 @@
 function threeSum(nums: number[]): number[][] {
-
-    let result = [[]]
-
-    
-
-    for(let i = 0 ; i < nums.length ; i++){
-        if(Math.pow(10,5) <= nums[i] || Math.pow(-10,5) <= nums[i] ){
-            let answer = []
-
-
     nums.sort((a, b) => a - b)
 
+    let answer = []
 
-    for(let i = 0 ; i < nums.length ; i++){
-        for(let j = i+1 ; j < nums.length ; j++){
-           for(let k = j+1 ; k < nums.length ; k++){
-                if(nums[i] + nums[j] + nums[k] === 0){
-                    answer.push([nums[i],nums[j],nums[k]])
+    let result = []
+
+    for (let i = 0; i < nums.length; i++) {
+        if (3 <= nums.length && nums.length <= 3000 && nums[i] >= -100000 && nums[i] <= 100000) {
+            for (let j = i + 1; j < nums.length; j++) {
+                for (let k = j + 1; k < nums.length; k++) {
+                    if (nums[i] + nums[j] + nums[k] === 0) {
+                        answer.push([nums[i], nums[j], nums[k]])
+                    }
                 }
-           }
-            
+            }
+
+            const data = new Set(answer.map(an => JSON.stringify(an)))
+            result = Array.from(data).map(str => JSON.parse(str))
+
+        } else {
+            break
         }
     }
 
-
-
-    const b = new Set(answer.map(an => JSON.stringify(an)))
-
-     result = Array.from(b).map(str => JSON.parse(str))
-    
-    
-        }
-        else if(3 <= nums.length || nums.length <= 3000){
-            let answer = []
-
-
-    nums.sort((a, b) => a - b)
-
-
-    for(let i = 0 ; i < nums.length ; i++){
-        for(let j = i+1 ; j < nums.length ; j++){
-           for(let k = j+1 ; k < nums.length ; k++){
-                if(nums[i] + nums[j] + nums[k] === 0){
-                    answer.push([nums[i],nums[j],nums[k]])
-                }
-           }
-            
-        }
-    }
-
-
-
-    const b = new Set(answer.map(an => JSON.stringify(an)))
-
-     result = Array.from(b).map(str => JSON.parse(str))
-    
-    
-        }
-    }
-    
     return result
 
 };
-console.log(threeSum([0,0,0]))
+console.log(threeSum([1,2,3]))
