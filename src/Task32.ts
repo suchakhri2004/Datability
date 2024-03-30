f77552b9ee9714ba74a05a2eb98abffb1f450132
suchakhri <suchakhri1992@gmail.com>
2024-03-30 20:36:13 +0700

    Update Task32.ts

--- src/Task32.ts
+++ src/Task32.ts
@@ -1,64 +1,30 @@
 function longestValidParentheses(s: string): number {
-
-    let ArrNum = Array.from(s)
-    let answer = 0
-    let answerSAVE = 0
-    let answerThree = 0
-
-    let Arr = []
-
-    let ArrNumV2 = []
-
-    
-
-
-    for (let i = 0; i < ArrNum.length; i++) {
-        const current = ArrNum[i]
-        const Next = ArrNum[i + 1]
-        if (current === '(' && Next === ')') {
-            ArrNum.splice(i, 2)
-            answer++
-            if (ArrNum[i] === '(' && ArrNum[i + 1] === '(' || ArrNum[i] === ')' && ArrNum[i + 1] === ')') {
-                answerSAVE = answer
-                answer = 0
-                Arr.push(ArrNum[i])
-                ArrNum.splice(i,1)                
-                break
+    let result = 0
+    let indexArr : number[] = [-1]
+    for(let i = 0 ; i < s.length ; i++ ){
+        if(s[i] === '('){
+            indexArr.push(i)
+            //ถ้าเจอ ( ให้ push indexของ ( เลย ละจบ หาตัวต่อไป
+        }
+        else{
+            indexArr.pop()
+            //ถ้าเจอ ) ให้ pop ออกละเช็คเงื่อนไขล่าง
+            if(indexArr.length === 0){
+                indexArr.push(i)
+                //ถ้าตัวใน indexArr.length === 0 ให้ push index ของ ) เข้าไป ละจบวนกลับขึ้นบน
+                //ถ้ามีก็เช็คเงื่อนไขต่อไป
+            }
+            else{
+                result = Math.max(result, i - indexArr[indexArr.length - 1 ])
+                //ถ้าตัวใน indexArr.length !== 0 ให้ result = ค่าที่มากที่สุดที่เลือกมาจาก Math.max
+                // โดยเลือกจาก result คือตัวมันที่เก็บมาตลอด หรือ ค่า index ล่าสุด - ตัวท้ายสุดของ indexArr
+                //ค่าไหนมากสุดก็เอาค่านั้นมาเก็บ ละวนกลับขึ้นไปด้านบน loop เช็คครบทุกตัว
             }
         }
     }
 
-    for (let i = ArrNum.length - 1 ; i >= 0 ; i--) {
-        if (ArrNum[i] === '(' && ArrNum[i + 1] === ')') {
-            ArrNum.splice(i, 2)
-            answer++
-        }
-    }
-
-    let answerReal = 0
-    
-    console.log("Arr ===>",Arr);
-    console.log("ArrNum ===>",ArrNum);
-    
-    if(Arr[0] === '(' && ArrNum[0] === ')'){
-        answerThree ++
-        answerReal = answer + answerSAVE + answerThree
-    }
-    else if(Arr[0]===')'){
-        answerReal = answer + answerSAVE
-    }
-    else{
-        answerReal = answer
-    }
-
-    console.log("answer",answer)
-    console.log("answerSave",answerSAVE)
-    console.log("answerThree",answerThree)
-    
-    
-
-    let result = answerReal * 2
-
     return result
 };
-console.log("Result ===>", longestValidParentheses(")()())()()("));
+console.log(longestValidParentheses('()))(()'));
+
+//ไม่ใช่แค่การเช็ค วงเล็บ แต่คือการเอา วงเล็บ มาคัดแยก หาตัวที่ วงเล็บ ติดกัน โดยไม่ถูกขัด จาก วงเล็บ ที่ไม่สมบูรณ์
