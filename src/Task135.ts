01f8112925368aad1c9543ed5e06e79acb11be60
suchakhri <suchakhri1992@gmail.com>
2024-04-01 17:10:21 +0700

    update

--- src/Task135.ts
+++ src/Task135.ts
@@ -38,5 +38,4 @@ function candy(ratings: number[]): number {
 console.log('count ===>', candy([1,3,4,5,2]));
 
 
-// 1 2 3 1 3 2 1
 //ลูกอมต้องมากกว่าคนข้างๆที่ ratings น้อยกว่าเสมอ ลูกอมเท่ากับ คนข้างๆ +1
